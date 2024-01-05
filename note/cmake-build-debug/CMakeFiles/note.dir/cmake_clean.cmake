e30f509e166fb7feefd183b8c7edb7041b99a1e7
KajetanFrackowiak <kajtek.gdynia@gmail.com>
2024-01-05 07:06:52 +0100

    .

---
+++ note/cmake-build-debug/CMakeFiles/note.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/note.dir/main.cpp.obj"
+  "CMakeFiles/note.dir/main.cpp.obj.d"
+  "libnote.dll.a"
+  "note.exe"
+  "note.exe.manifest"
+  "note.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/note.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- note/main.cpp
+++ note/main.cpp
@@ -28,13 +28,37 @@ public:
     }
 };
 
-int main() {
-    YouToubeChannel ytChannel("CodeBeauty", "Saldina");
-    ytChannel.PublishVideo("C++ for begginers");
-    ytChannel.PublishVideo("HTML & CSS for beginners");
-    ytChannel.PublishVideo("C++ OOP");
-    ytChannel.Subscribe();
-    ytChannel.GetInfo();
+int getMin(int numbers[], int size) {
+    int min = numbers[0];
+    for (int i = 1; i < size; i++) {
+        if (numbers[i] < min) {
+            min = numbers[i];
+        }
+    }
+    return min;
+}
 
-    system("pause>0");
+int getMax(const int numbers[], int size) {
+    int max = numbers[0];
+    for (int i = 1; i < size; i++) {
+        if (numbers[i] > max) {
+            max = numbers[i];
+        }
+    }
+    return max;
+}
+
+int main() {
+    // YouToubeChannel ytChannel("CodeBeauty", "Saldina");
+    // ytChannel.PublishVideo("C++ for begginers");
+    // ytChannel.PublishVideo("HTML & CSS for beginners");
+    // ytChannel.PublishVideo("C++ OOP");
+    // ytChannel.Subscribe();
+    // ytChannel.GetInfo();
+
+    int numbers[5] = {5,4,-2,29,6};
+    std::cout << "Min is: " << getMin(numbers, 5) << std::endl;
+    std::cout << "Max is: " << getMax(numbers, 5) << std::endl;
+    // system("pause>0");
+    return 0;
 }
